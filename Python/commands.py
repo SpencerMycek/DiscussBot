ea9b69eed0eaef562777f3255a4a9d924e6d4f90
Spencer Mycek <Spence.Mycek@gmail.com>
2019-03-21 15:57:40 -0400

    Add code to delegate commands in and out of relevent channel

--- Python/commands.py
+++ Python/commands.py
@@ -2,7 +2,24 @@
 A file to contain all commands and handle command delegations
 Author: Spencer Mycek
 """
-import requests
+import requests, re
+
+
+MENTION_REGEX="^<@(|[WU][^>]+)>(.*)"
+
+"""
+Handles all messages in the "discussion" channel
+and formats the discussion with the user_token
+"""
+def format_discussion(bot_token, user_token, message):
+    pass
+
+
+"""
+Handles all messages in all channels not "discussion"
+"""
+def commands_elsewhere(bot_token, user_token, message):
+    pass
 
 
 """
@@ -10,7 +27,8 @@ Master method that redirects different message inputs to different commands
 """
 def master_command(bot_token, user_token , bot_id, channel_id, message):
     if message['channel'] == channel_id:
-        pass
+        format_discussion(bot_token, user_token,  message)
     else:
-        pass
-
+        matches = re.match(MENTION_REGEX, message['text'])
+        if matches.group(1) == bot_id:
+            commands_elsewhere(bot_token, user_token, message)
