47ce30a92bda2722b7c43363fef3ebbf7b26cce0
Spencer Mycek <srm2686@rit.edu>
2019-04-08 15:33:13 -0400

    Moving Computers

--- src/Discussion/discuss_speaker.py
+++ src/Discussion/discuss_speaker.py
@@ -6,13 +6,13 @@ Author: Spencer Mycek
 # A dictionary that keeps track of "topic":List of 'new points'
 # Each new point will have a list of direct response authors at [0] and original author at [1] and a thread_ts at [2]
 # Example:
-#   { 'topic':[ 
-#               [ 
+#   { 'topic':[
+#               [
 #                   ['P1','P2'] , 'P3' , thread_ts
-#               ], 
-#               [ 
+#               ],
+#               [
 #                   [ 'P3', 'P1' ] , 'P2' , thread_ts
-#               ]  
+#               ]
 #           ]
 #   {
 #
@@ -24,7 +24,8 @@ current_topic = "Default Topic"
 def add_topic(topic):
     """Create a new topic"""
     global discussion_list
-    discussion_list[str(topic).strip] = []  # Creates the first new point with the creator of the topic as the author
+    discussion_list[str(topic).strip] = [
+    ]  # Creates the first new point with the creator of the topic as the author
 
 
 def add_new_point(author, ts):
@@ -37,7 +38,14 @@ def add_direct_response(author, target):
     """Add a direct response to the target new point based on the author"""
     global discussion_list
     for x in discussion_list[current_topic]:
-        print(x[1])
+        r = requests.get(
+            'https://slack.com/api/users.info',
+            data={
+                'token': target,
+                'user': x
+            })
+        print(r.json()['user']['name'])
+
 
 def get_topics():
     """Returns a list of topics in the discussion as a string"""
@@ -48,13 +56,13 @@ def get_topics():
     for topic in discussion_list.keys():
         if topic is current_topic:
             continue
-        result += "Topic {}: {}\n".format(str(i+1), topic)
-        i+=1
+        result += "Topic {}: {}\n".format(str(i + 1), topic)
+        i += 1
 
     return result
+
 
 def get_discussion():
     """Returns the current discusion"""
     global discussion_list
     return discussion_list[current_topic]
-
